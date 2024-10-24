9d915f510bb82d019237ba1a028e00e0c3e0f822
Mahdi777-Hex <mahdi.1111lahbali@gmail.com>
2024-10-24 03:10:14 +0100

    Add files via upload

---
+++ NEW KEY BY SH4KO DYLIB.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="Sh4KoO" 
+ while (sWord != "ZINGO_IOS") {// Pass
+ sWord = prompt("𝘌𝘯𝘵𝘦𝘳 𝘛𝘩𝘦 𝘒𝘦𝘺 :")  
+ } 
+ 
+//confirm("ok");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 ZINGO IOS  ");
+
+ } 
